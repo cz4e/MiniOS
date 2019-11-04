8ac2f3f5e1d687d06a49735c3bede517e9da6fcd
Lyn-Wu <15155930562@163.com>
2019-11-04 20:37:06 +0800

    modofied mips's kern_pcb.c

--- MiniOS/src/mips/kern/kern_pcb.c
+++ MiniOS/src/mips/kern/kern_pcb.c
@@ -1,10 +1,10 @@
 #include <mips/include/pcb.h>
 
-extern void _pcb_regs_init(struct cpuset *);
+extern void _pcb_regs_init(struct cpu_regs *);
 
 void
 pcb_init(struct pcb *pcb,void *onfault)
 {
-    _pcb_regs_init(&(pcb->pcb_cpuset));
+    _pcb_regs_init(&(pcb->pcb_cpuset.cs_regs));
     pcb->cs_onfault = onfault;
 }
