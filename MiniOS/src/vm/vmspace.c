dcd297171b4240e260839a322c15f3ba360e0dfc
Lyn-Wu <15155930562@163.com>
2019-11-11 21:36:54 +0800

    modified vmspace.c

--- MiniOS/src/vm/vmspace.c
+++ MiniOS/src/vm/vmspace.c
@@ -3,8 +3,10 @@
 #include <vm/zone.h>
 #include <vm/node.h>
 #include <vm/vmspace.h>
+#include <sys/lock.h>
 
-static struct vmspace node_pool[MAX_VMSPACE_POOL];
+static struct vmspace vmspace_pool[MAX_VMSPACE_POOL];
+static struct mtx vmspace_lock;
 
 static int 
 is_round_base2(uint64_t n)
@@ -43,14 +45,7 @@ round_base2(uint64_t n)
     return (f == 1 ? 2 : f);
 }
 
-static vm_addr_t
-vmspace_find_buddy(vmspace_t vmspace)
-{
-    vm_addr_t buddy_address;
-    buddy_address = vmspace->vm_start + vmspace->vm_initial_size;
 
-    return (buddy_address);
-}
 
 vmspace_t
 vmspace_alloc(zone_t zone, vm_size_t size)
@@ -64,6 +59,28 @@ vmspace_alloc(zone_t zone, vm_size_t size)
     ZONE_UNLOCK(&zone->zone_lock);
     return (res);
 }
+
+static vmspace_t
+find_a_vaild_vmspace(void)
+{
+    vmspace_t res = NULL;
+    static int  index = 0;
+
+    for(;;) {
+        if(index < MAX_VMSPACE_POOL) {
+            if(vmspace_pool[index].vm_present_size == 0 
+                        && vmspace_pool[index].vm_initial_size == 0) {
+                res = &vmspace_pool[index];
+                break;
+            } else {
+                index = 0;
+            }
+        }
+    }
+
+    return (res);
+}
+
 
 static vmspace_t
 _vmspace_alloc(zone_t zone, int order, vm_size_t size)
@@ -86,7 +103,7 @@ _vmspace_alloc(zone_t zone, int order, vm_size_t size)
         panic("_vmspace_alloc: kernel resources exhausted");
     }
 
-    res = vmspace_node_select_and_divide(zone, present_node, size);
+    res = vmspace_node_select_and_divide(zone, present_node,order, size);
 
     return (res);
 }
@@ -102,16 +119,21 @@ vmspace_node_select_and_divide(zone_t zone, node_t node,int  order, vm_size_t si
     int reserve_pages, page_counter = 0;
     vmspace_t new_node;
 
+/*
+    Start allocation
+*/
+    res = find_a_vaild_vmspace(); /* get a vaild structure from pool */
     LIST_NEXT(res,vm_entry) = LIST_PREV(res,vm_entry) = NULL;
 
     source_space = LIST_FIRST(&node->node_vmspace);
     LIST_REMOVE_HEAD(&node->node_vmspace,source_space,vm_entry);
     node->node_nr_nodes--;
 
+    total = (size / PAGE_SIZE + 1) ;    /* pages */
+    res->vm_present_size = total * PAGE_SIZE;
     res->vm_start = source_space->vm_start;
     res->vm_owner = source_space->vm_owner;
 
-    total = (size / PAGE_SIZE + 1) ;
     reserve_pages = (powerof2(order) - round_base2(total)) / PAGE_SIZE;
 
 #ifdef MEM_GROUW_DOWN
@@ -120,6 +142,12 @@ vmspace_node_select_and_divide(zone_t zone, node_t node,int  order, vm_size_t si
     source_space->vm_start += total * PAGE_SIZE;
 #endif
 
+/*
+    Here, allocation is done!
+*/
+/*
+    Insert reserve pages into anoter order list
+*/
     if(is_round_base2(reserve_pages)) {
         reserve_pages = round_base2(reserve_pages);
         LIST_INSERT_HEAD(&zone->zone_nodes[reserve_pages]->node_vmspace,source_space,vm_entry);
@@ -130,26 +158,25 @@ vmspace_node_select_and_divide(zone_t zone, node_t node,int  order, vm_size_t si
         reserve_pages--;
         page_counter++;
         if(is_round_base2(reserve_pages)) {
-            new_node = find_a_vaild_node();
-            new_node->vm_start = source_space->vm_start;
-            new_node->vm_present_size = page_counter * PAGE_SIZE;
+            new_node = find_a_vaild_vmspace();
+#ifdef MEM_GROW_DOWN
+            new_node->vm_start = source_space->vm_start - PAGE_SIZE * page_counter;
+#else
+            new_node->vm_start = source_space->vm_start + PAGE_SIZE * page_counter;
+#endif
+            new_node->vm_present_size = reserve_pages * PAGE_SIZE;
             new_node->vm_initial_size = source_space->vm_initial_size;
             new_node->vm_owner = source_space->vm_owner;
-#ifdef MEM_GROUW_DOWN
-            source_space->vm_start -= page_counter * PAGE_SIZE;
-#else
-            source_space->vm_start += page_counter * PAGE_SIZE;
-#endif
-            reserve_pages = round_base2(reserve_pages);
-            page_counter = round_base2(page_counter);
-            zone->zone_nodes[reserve_pages]->node_nr_nodes++;
-            zone->zone_nodes[page_counter]->node_nr_nodes++;
+            source_space->vm_present_size -= reserve_pages * PAGE_SIZE;
 
-            LIST_INSERT_HEAD(&zone->zone_nodes[reserve_pages]->node_vmspace,source_space,vm_entry);
-            LIST_INSERT_HEAD(&zone->zone_nodes[page_counter]->node_vmspace,new_node,vm_entry);
-            goto success;
+            reserve_pages = round_base2(reserve_pages);
+            zone->zone_nodes[reserve_pages]->node_nr_nodes++;
+
+            LIST_INSERT_HEAD(&zone->zone_nodes[reserve_pages]->node_vmspace,new_node,vm_entry);
+            reserve_pages = page_counter;
+            page_counter = 0;
         } else if(reserve_pages == 0) { /* only one page reserve */
-            new_node = find_a_vaild_node();
+            new_node = find_a_vaild_vmspace();
             new_node->vm_start = source_space->vm_start;
             new_node->vm_present_size = PAGE_SIZE;
             new_node->vm_initial_size = source_space->vm_initial_size;
@@ -159,7 +186,9 @@ vmspace_node_select_and_divide(zone_t zone, node_t node,int  order, vm_size_t si
             LIST_INSERT_HEAD(&zone->zone_nodes[0]->node_vmspace,new_node,vm_entry);
         }
     }
-    
+/*
+    Insert finished!
+*/
 success:
 
     return (res);
@@ -169,11 +198,36 @@ success:
 vmspace_t 
 _vmspace_alloc_wait(zone_t zone, int order, vm_size_t size)
 {
+    node_t present_node;
+    vmspace_t res;
 
+    if(order >= MAX_ORDER || order < 0) {
+        panic("_vmspace_alloc: the order must less than MAX_ORDER and great than zero");
+    }
+
+    present_node = zone->zone_nodes[order];
+
+    while(1) {
+        while(order < MAX_ORDER && present_node->node_nr_nodes == 0) {
+            present_node = zone->zone_nodes[order];
+                ++order;
+        }
+
+        if(order == MAX_ORDER) {
+            ZONE_UNLOCK(&zone->zone_lock);
+            // sleep a little while;
+        } else {
+            break;
+        }
+    }
+
+    res = vmspace_node_select_and_divide(zone, present_node,order, size);
+
+    return (res);
 }
 
 void
 vmspace_free(zone_t zone, vmspace_t vmspace)
 {
-
+    uint64_t size = vmspace->vm_present_size;
 }
